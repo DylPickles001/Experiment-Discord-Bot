10becdade55648a6d5d9bd3de6f8e92b027617da
DylPickles001 <nguyen.dylan142@gmail.com>
2020-12-20 19:19:29 +0000

    Installed Discord files and added basic message response to  command

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import discord
+import os
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}                    '.format(client))
+
+
+#Action made by bot
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return 
+
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello!')
+
+#Runs bot
+client.run(os.getenv('TOKEN'))
